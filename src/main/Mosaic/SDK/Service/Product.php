fbdf8ec5a58c14eb863fec5a5359bc0eaab82013
Kore Nordmann <mail@kore-nordmann.de>
2012-10-30 09:15:37 +0100

    Stubbed product service

---
+++ src/main/Mosaic/SDK/Service/Product.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file is part of the Mosaic SDK Component.
+ *
+ * @version $Revision$
+ */
+
+namespace Mosaic\SDK\Service;
+
+use Mosaic\SDK\Gateway;
+
+/**
+ * Product service
+ *
+ * @version $Revision$
+ */
+class Product
+{
+    /**
+     * Gateway to changes feed
+     *
+     * @var Gateway
+     */
+    protected $gateway;
+
+    /**
+     * COnstruct from gateway
+     *
+     * @param Gateway $gateway
+     * @return void
+     */
+    public function __construct(Gateway $gateway)
+    {
+        $this->gateway = $gateway;
+    }
+
+    public function export($productCount)
+    {
+        return $this->gateway->getNextChanges($productCount);
+    }
+}
