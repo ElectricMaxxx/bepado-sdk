3df08f8718d7cd769c649a4f09cf15d4b4de4384
Kore Nordmann <mail@kore-nordmann.de>
2012-11-05 14:39:05 +0100

    Stubbed controller methods

--- src/main/Mosaic/SDK/Controller.php
+++ src/main/Mosaic/SDK/Controller.php
@@ -66,4 +66,60 @@ class Controller
             )
         );
     }
+
+    /**
+     * Check products still are in the state they are stored locally
+     *
+     * This method will verify with the remote shops that products are still in
+     * the expected state. If the state of products changed this method will
+     * return a Struct\Message, which should be ACK'ed by the user. Otherwise
+     * this method will jsut return true.
+     *
+     * @param Struct\RemoteProduct[] $products
+     * @return mixed
+     */
+    public function checkProducts(array $products)
+    {
+        throw new \RuntimeException('@TODO: Implement');
+    }
+
+    /**
+     * Reserve products
+     *
+     * This method will reserve the given products in the remote shops.
+     *
+     * If the product data change in a relevant way, this method will not
+     * reserve the products, but instead return a Struct\Message, which should
+     * be ACK'ed by the user. Afterwards another reservation may be issued.
+     *
+     * If The reservation of the product set succeeded a hash of reservation
+     * IDs for all involved shops will be returned. This hash must be stored in
+     * the shop for all further transactions. The session is probably the best
+     * location for this.
+     *
+     * @param Struct\RemoteProduct[] $products
+     * @return mixed
+     */
+    public function reserveProducts(array $products)
+    {
+        throw new \RuntimeException('@TODO: Implement');
+    }
+
+    /**
+     * Checkout product sets related to the given reservation IDs
+     *
+     * This process is the final "buy" transaction. It should be part of the
+     * checkout process and be handled synchronously.
+     *
+     * This method will jsut return true, if the transaction worked as
+     * expected. If it failed, or partially failed, a corresponding
+     * Struct\Message will be returned.
+     *
+     * @param string[] $products
+     * @return mixed
+     */
+    public function checkout(array $reservationIDs)
+    {
+        throw new \RuntimeException('@TODO: Implement');
+    }
 }
