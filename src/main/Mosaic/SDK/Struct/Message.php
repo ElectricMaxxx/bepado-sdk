0f69aa30f46429597bb371874dee0b5cfcf4a43b
Kore Nordmann <mail@kore-nordmann.de>
2012-11-06 12:41:25 +0100

    Added: message struct

---
+++ src/main/Mosaic/SDK/Struct/Message.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is part of the Mosaic SDK Component.
+ *
+ * @version $Revision$
+ */
+
+namespace Mosaic\SDK\Struct;
+
+use Mosaic\SDK\Struct;
+
+/**
+ * Struct class representing a message
+ *
+ * @version $Revision$
+ * @api
+ */
+class Message extends Struct
+{
+    /**
+     * @var string
+     */
+    public $message;
+}
