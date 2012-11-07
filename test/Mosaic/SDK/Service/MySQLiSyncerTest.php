f54204e598b93578c02bbc20df09bafeb635fd3b
Kore Nordmann <mail@kore-nordmann.de>
2012-11-07 09:12:40 +0100

    Test with both gateway implementations

---
+++ test/Mosaic/SDK/Service/MySQLiSyncerTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * This file is part of the Mosaic SDK Component.
+ *
+ * @version $Revision$
+ */
+
+namespace Mosaic\SDK\Service;
+
+use Mosaic\Common;
+use Mosaic\SDK;
+
+require_once __DIR__ . '/SyncerTest.php';
+
+class MySQLiSyncerTest extends SyncerTest
+{
+    /**
+     * Get used gateway for test
+     *
+     * @return SDK\Gateway
+     */
+    protected function getGateway()
+    {
+        if ($this->gateway) {
+            return $this->gateway;
+        }
+
+        $config = @parse_ini_file(__DIR__ . '/../../../../build.properties');
+        $connection = new \Mosaic\SDK\MySQLi(
+            $config['db.hostname'],
+            $config['db.userid'],
+            $config['db.password'],
+            $config['db.name']
+        );
+        $connection->query('TRUNCATE TABLE mosaic_change;');
+        $connection->query('TRUNCATE TABLE mosaic_product;');
+
+        return $this->gateway = new SDK\Gateway\MySQLi($connection);
+    }
+}
--- test/Mosaic/SDK/Service/SyncerTest.php
+++ test/Mosaic/SDK/Service/SyncerTest.php
@@ -9,31 +9,20 @@ namespace Mosaic\SDK\Service;
 
 use Mosaic\Common;
 use Mosaic\SDK;
+use Mosaic\SDK\Struct\Change;
 
 require_once __DIR__ . '/../bootstrap.php';
 
-class SyncerTest extends Common\Test\TestCase
+abstract class SyncerTest extends Common\Test\TestCase
 {
     protected $gateway;
 
-    protected function getMySQLiGateway()
-    {
-        if ($this->gateway) {
-            return $this->gateway;
-        }
-
-        $config = @parse_ini_file(__DIR__ . '/../../../../build.properties');
-        $connection = new \Mosaic\SDK\MySQLi(
-            $config['db.hostname'],
-            $config['db.userid'],
-            $config['db.password'],
-            $config['db.name']
-        );
-        $connection->query('TRUNCATE TABLE mosaic_change;');
-        $connection->query('TRUNCATE TABLE mosaic_product;');
-
-        return $this->gateway = new SDK\Gateway\MySQLi($connection);
-    }
+    /**
+     * Get used gateway for test
+     *
+     * @return SDK\Gateway
+     */
+    abstract protected function getGateway();
 
     /**
      * Get product provider
@@ -87,7 +76,10 @@ class SyncerTest extends Common\Test\TestCase
             array_map(
                 function ($change)
                 {
-                    return array($change->operation, $change->sourceId);
+                    // The revision changes in every run
+                    $change = clone $change;
+                    $change->revision = null;
+                    return $change;
                 },
                 $changes
             )
@@ -97,7 +89,7 @@ class SyncerTest extends Common\Test\TestCase
     public function testInitialBuild()
     {
         $syncer = new SDK\Service\Syncer(
-            $gateway = $this->getMySQLiGateway(),
+            $gateway = $this->getGateway(),
             $this->getProductProvider(array(1, 2)),
             new SDK\RevisionProvider\Time(),
             new SDK\ProductHasher\Simple()
@@ -106,8 +98,8 @@ class SyncerTest extends Common\Test\TestCase
 
         $this->assertChanges(
             array(
-                array('insert', 1),
-                array('insert', 2),
+                new Change\Insert(array('sourceId' => '1')),
+                new Change\Insert(array('sourceId' => '2')),
             ),
             $changes = $gateway->getNextChanges(null, 100)
         );
@@ -121,7 +113,7 @@ class SyncerTest extends Common\Test\TestCase
     {
         $revision = $this->testInitialBuild();
         $syncer = new SDK\Service\Syncer(
-            $gateway = $this->getMySQLiGateway(),
+            $gateway = $this->getGateway(),
             $this->getProductProvider(array(1, 2)),
             new SDK\RevisionProvider\Time(),
             new SDK\ProductHasher\Simple()
@@ -135,13 +127,13 @@ class SyncerTest extends Common\Test\TestCase
     }
 
     /**
-     * @depends testInitialBuild
+     * @depends testReIndex
      */
     public function testProductUpdate()
     {
         $revision = $this->testInitialBuild();
         $syncer = new SDK\Service\Syncer(
-            $gateway = $this->getMySQLiGateway(),
+            $gateway = $this->getGateway(),
             $this->getProductProvider(array(1, 2), 'update'),
             new SDK\RevisionProvider\Time(),
             new SDK\ProductHasher\Simple()
@@ -150,21 +142,21 @@ class SyncerTest extends Common\Test\TestCase
 
         $this->assertChanges(
             array(
-                array('update', 1),
-                array('update', 2),
+                new Change\Update(array('sourceId' => '1')),
+                new Change\Update(array('sourceId' => '2')),
             ),
             $gateway->getNextChanges($revision, 100)
         );
     }
 
     /**
-     * @depends testInitialBuild
+     * @depends testReIndex
      */
     public function testProductDelete()
     {
         $revision = $this->testInitialBuild();
         $syncer = new SDK\Service\Syncer(
-            $gateway = $this->getMySQLiGateway(),
+            $gateway = $this->getGateway(),
             $this->getProductProvider(array()),
             new SDK\RevisionProvider\Time(),
             new SDK\ProductHasher\Simple()
@@ -173,8 +165,8 @@ class SyncerTest extends Common\Test\TestCase
 
         $this->assertChanges(
             array(
-                array('delete', 1),
-                array('delete', 2),
+                new Change\Delete(array('sourceId' => '1')),
+                new Change\Delete(array('sourceId' => '2')),
             ),
             $gateway->getNextChanges($revision, 100)
         );
