c6ec49c13fa712e33db98301d4c2f9f9313ecd41
Kore Nordmann <mail@kore-nordmann.de>
2012-11-08 11:54:49 +0100

    Added: Shop configuration gateway interface

---
+++ src/main/Mosaic/SDK/Gateway/ShopConfiguration.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * This file is part of the Mosaic SDK Component.
+ *
+ * @version $Revision$
+ */
+
+namespace Mosaic\SDK\Gateway;
+
+use Mosaic\SDK\Struct;
+
+/**
+ * Gateaway interface to maintain shop configurations
+ *
+ * @version $Revision$
+ * @api
+ */
+interface ShopConfiguration
+{
+    /**
+     * Update shop configuration
+     *
+     * @param string $shopId
+     * @param Struct\ShopConfiguration $configuration
+     * @return void
+     */
+    public function setShopConfiguration($shopId, Struct\ShopConfiguration $configuration);
+
+    /**
+     * Get configuration for the given shop
+     *
+     * @param string $shopId
+     * @return Struct\ShopConfiguration
+     */
+    public function getShopConfiguration($shopId);
+}
