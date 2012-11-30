47356befa966221fdfbac8cdddbab74ad5be4df4
Kore Nordmann <mail@kore-nordmann.de>
2012-11-30 16:03:03 +0100

    Stubbed verification service integration

---
+++ test/Mosaic/SDK/Service/VerificationTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of the Mosaic SDK Component.
+ *
+ * @version $Revision$
+ */
+
+namespace Mosaic\SDK\Service;
+
+use Mosaic\Common;
+use Mosaic\SDK;
+
+require_once __DIR__ . '/../bootstrap.php';
+
+class VerificationTest extends Common\Test\TestCase
+{
+    public function testVerify()
+    {
+        $this->markTestIncomplete('@TODO: Implement test');
+    }
+}
