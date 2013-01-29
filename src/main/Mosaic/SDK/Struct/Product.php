9f3b2a7b3437e3662393d3efe93a97c289d89023
Kore Nordmann <mail@kore-nordmann.de>
2013-01-29 09:21:19 +0100

    Documented product properties

--- src/main/Mosaic/SDK/Struct/Product.php
+++ src/main/Mosaic/SDK/Struct/Product.php
@@ -18,71 +18,119 @@ use Mosaic\SDK\Struct;
 class Product extends Struct
 {
     /**
+     * Local ID of the product in your shop.
+     *
+     * ID should never change for one product or be reused for another product.
+     *
+     * @var string
+     */
+    public $sourceId;
+
+    /**
+     * URL to the product in your shop.
+     *
+     * Used for redirects to the product, or views of the product.
+     *
+     * @var string
+     */
+    public $url;
+
+    /**
+     * Title / name of the product
+     *
+     * @var string
+     */
+    public $title;
+
+    /**
+     * A short description of the product
+     *
+     * May contain simple HTML
+     *
+     * @var string
+     */
+    public $shortDescription;
+
+    /**
+     * An extensive / full description of the product
+     *
+     * May contain simple HTML
+     *
+     * @var string
+     */
+    public $longDescription;
+
+    /**
+     * Name of the product vendor
+     *
+     * @var string
+     */
+    public $vendor;
+
+    /**
+     * Current price of the product.
+     *
+     * Provided as a float.
+     *
+     * @var float
+     */
+    public $price;
+
+    /**
+     * Currency of the price
+     *
+     * Currently only the default "EUR" is supported.
+     *
+     * @var string
+     */
+    public $currency = "EUR";
+
+    /**
+     * Availibility of the product
+     *
+     * Provide an integer with the amount of products currently in stock and
+     * ready for delivery.
+     *
+     * @var integer
+     */
+    public $availability;
+
+    /**
+     * List of product image URLs
+     *
+     * @var string[][]
+     */
+    public $images = array();
+
+    /**
+     * Product categories.
+     *
+     * For a full list of currently available product categories call
+     * getCategories() on the SDK class.
+     *
+     * @var string[]
+     */
+    public $categories = array();
+
+    /**
+     * ID of the shop.
+     *
+     * Will be set by the SDK.
+     *
      * @var string
      * @access private
      */
     public $shopId;
 
     /**
-     * @var string
-     */
-    public $sourceId;
-
-    /**
+     * Product revision
+     *
+     * Will be set by the SDK.
+     *
      * @var string
      * @access private
      */
     public $revisionId;
-
-    /**
-     * @var string
-     */
-    public $url;
-
-    /**
-     * @var string
-     */
-    public $title;
-
-    /**
-     * @var string
-     */
-    public $shortDescription;
-
-    /**
-     * @var string
-     */
-    public $longDescription;
-
-    /**
-     * @var string
-     */
-    public $vendor;
-
-    /**
-     * @var float
-     */
-    public $price;
-
-    /**
-     * @var string
-     */
-    public $currency;
-
-    /**
-     * @var integer
-     */
-    public $availability;
-
-    /**
-     * @var string[][]
-     */
-    public $images = array();
-
-    /**
-     * @var string[]
-     */
-    public $categories = array();
 
     /**
      * Restores a product from a previously stored state array.
