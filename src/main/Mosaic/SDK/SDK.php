b82960e1cf89623f3520fb3d33c404f5c26a8d01
Kore Nordmann <mail@kore-nordmann.de>
2013-01-30 10:47:41 +0100

    Moved response struct to Common component

--- src/main/Mosaic/SDK/SDK.php
+++ src/main/Mosaic/SDK/SDK.php
@@ -8,6 +8,7 @@
 namespace Mosaic\SDK;
 
 use Mosaic\Common\Struct\RpcCall;
+use Mosaic\Common\Struct\Response;
 
 /**
  * Central SDK class, which serves as an etnry point and service fromShop.
@@ -118,12 +119,13 @@ final class SDK
                     'service' => 'null',
                     'command' => 'return',
                     'arguments' => array(
-                        new Struct\Response(
+                        new Response(
                             array(
                                 'result' => $this->dependencies->getServiceRegistry()->dispatch(
                                     $this->dependencies->getUnmarshaller()->unmarshal($xml)
                                 ),
                                 'metrics' => $this->dependencies->getMetricService()->getMetrics(),
+                                'version' => self::VERSION,
                             )
                         )
                     )
