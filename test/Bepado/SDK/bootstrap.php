6dcbe610a52a7e3e5d599a59ff16a1d66248bcc0
Tobias Schlitt <toby@php.net>
2014-03-25 08:20:20 +0100

    Updated: Removed deprecated test boostrap.

--- test/Bepado/SDK/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * This file is part of the Bepado SDK Component.
- *
- * The SDK is licensed under MIT license. (c) Shopware AG and Qafoo GmbH
- */
-
-require __DIR__ . '/../../../vendor/autoload.php';
