b11da5f450491a936fe167b60f7a26ffca92c3d6
Kore Nordmann <mail@kore-nordmann.de>
2014-04-10 11:42:48 +0200

    PDO cannot be mocked – use SQLite in-memory DB instead

--- test/Bepado/SDK/SDKBuilderTest.php
+++ test/Bepado/SDK/SDKBuilderTest.php
@@ -12,7 +12,7 @@ class SDKBuilderTest extends \PHPUnit_Framework_TestCase
         $builder
             ->setApiKey('foo')
             ->setApiEndpointUrl('http://foo/bar')
-            ->configurePDOGateway(\Phake::mock('PDO'))
+            ->configurePDOGateway(new PDO('sqlite::memory:'))
             ->setProductToShop(\Phake::mock('Bepado\SDK\ProductToShop'))
             ->setProductFromShop(\Phake::mock('Bepado\SDK\ProductFromShop'))
             ->setErrorHandler(\Phake::mock('Bepado\SDK\ErrorHandler'))
@@ -30,7 +30,7 @@ class SDKBuilderTest extends \PHPUnit_Framework_TestCase
         $builder
             ->setApiKey('foo')
             ->setApiEndpointUrl('http://foo/bar')
-            ->configurePDOGateway(\Phake::mock('PDO'))
+            ->configurePDOGateway(new PDO('sqlite::memory:'))
             ->setProductToShop(\Phake::mock('Bepado\SDK\ProductToShop'))
             ->setProductFromShop(\Phake::mock('Bepado\SDK\ProductFromShop'))
         ;
