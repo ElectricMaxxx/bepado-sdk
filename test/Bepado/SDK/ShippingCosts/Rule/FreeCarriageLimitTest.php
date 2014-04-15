04e0e72c6f630359607aabef1b5f8e58939f0655
Benjamin Eberlei <kontakt@beberlei.de>
2014-04-15 18:59:02 +0200

    [BEP-415] Add rule for free carriage limits.

---
+++ test/Bepado/SDK/ShippingCosts/Rule/FreeCarriageLimitTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Bepado\SDK\ShippingCosts\Rule;
+
+use Bepado\SDK\Struct;
+use Phake;
+
+class FreeCarriageLimitTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_calculates_free_costs_when_order_total_exceeds_limit()
+    {
+        $delegatee = Phake::mock('Bepado\SDK\ShippingCosts\Rule');
+
+        $rule = new FreeCarriageLimit(array(
+            'freeLimit' => 100,
+            'delegatee' => $delegatee
+        ));
+
+        $this->assertEquals(0, $rule->getShippingCosts(
+            new Struct\Order(array(
+                'orderItems' => array(
+                    new Struct\OrderItem(array(
+                        'count' => 2,
+                        'product' => new Struct\Product(array(
+                            'purchasePrice' => 20,
+                            'vat' => 0.19
+                        ))
+                    )),
+                    new Struct\OrderItem(array(
+                        'count' => 3,
+                        'product' => new Struct\Product(array(
+                            'purchasePrice' => 20,
+                            'vat' => 0.19
+                        ))
+                    )),
+                )
+            ))
+        ));
+
+        \Phake::verifyNoInteraction($delegatee);
+    }
+
+    /**
+     * @test
+     */
+    public function it_delegates_shipping_costs_when_limit_not_exceeded()
+    {
+        $delegatee = Phake::mock('Bepado\SDK\ShippingCosts\Rule');
+        Phake::when($delegatee)->getShippingCosts(Phake::anyParameters())->thenReturn(10);
+
+        $rule = new FreeCarriageLimit(array(
+            'freeLimit' => 100,
+            'delegatee' => $delegatee
+        ));
+
+        $this->assertEquals(10, $rule->getShippingCosts(
+            new Struct\Order(array(
+                'orderItems' => array(
+                    new Struct\OrderItem(array(
+                        'count' => 1,
+                        'product' => new Struct\Product(array(
+                            'purchasePrice' => 10,
+                            'vat' => 0.19
+                        ))
+                    )),
+                )
+            ))
+        ));
+    }
+}
