102d5a27fc25e46c6aebb69c906898b332ea5820
Benjamin Eberlei <kontakt@beberlei.de>
2014-04-15 18:27:28 +0200

    [BEP-415] Add WeightDecorator Shipping cost rule.

---
+++ test/Bepado/SDK/ShippingCosts/Rule/WeightDecoratorTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Bepado\SDK\ShippingCosts\Rule;
+
+use Bepado\SDK\Struct;
+use Phake;
+
+class WeightDecoratorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_is_applicable_when_weight_is_less()
+    {
+        $delegatee = Phake::mock('Bepado\SDK\ShippingCosts\Rule');
+        Phake::when($delegatee)->isApplicable(\Phake::anyParameters())->thenReturn(true);
+
+        $weight = new WeightDecorator(array(
+            'maxWeight' => 10,
+            'delegatee' => $delegatee
+        ));
+
+        $this->assertTrue(
+            $weight->isApplicable(
+                new Struct\Order(
+                    array(
+                        'orderItems' => array(
+                            new Struct\OrderItem(array(
+                                'count' => 1,
+                                'product' => new Struct\Product(array(
+                                    'attributes' => array(
+                                        Struct\Product::ATTRIBUTE_WEIGHT => 2
+                                    )
+                                ))
+                            )),
+                            new Struct\OrderItem(array(
+                                'count' => 1,
+                                'product' => new Struct\Product(array(
+                                    'attributes' => array(
+                                        Struct\Product::ATTRIBUTE_WEIGHT => 3
+                                    )
+                                ))
+                            )),
+                        )
+                    )
+                )
+            )
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_is_not_applicable_when_big_count_is_larger_max_weight()
+    {
+        $delegatee = Phake::mock('Bepado\SDK\ShippingCosts\Rule');
+        Phake::when($delegatee)->isApplicable(\Phake::anyParameters())->thenReturn(true);
+
+        $weight = new WeightDecorator(array(
+            'maxWeight' => 10,
+            'delegatee' => $delegatee
+        ));
+
+        $this->assertFalse(
+            $weight->isApplicable(
+                new Struct\Order(
+                    array(
+                        'orderItems' => array(
+                            new Struct\OrderItem(array(
+                                'count' => 10,
+                                'product' => new Struct\Product(array(
+                                    'attributes' => array(
+                                        Struct\Product::ATTRIBUTE_WEIGHT => 2
+                                    )
+                                ))
+                            )),
+                        )
+                    )
+                )
+            )
+        );
+    }
+}
