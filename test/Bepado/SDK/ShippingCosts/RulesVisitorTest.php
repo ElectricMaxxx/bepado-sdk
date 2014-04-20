cba6b3b029a3e21d8a32404904a85ede4f648682
Benjamin Eberlei <kontakt@beberlei.de>
2014-04-20 10:22:45 +0200

    [BEP-415] Add visitor to make converting rules to other representations simpler.

---
+++ test/Bepado/SDK/ShippingCosts/RulesVisitorTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Bepado\SDK\ShippingCosts;
+
+use Phake;
+
+class RulesVisitorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_recursively_visits_rules()
+    {
+        $visitor = \Phake::partialMock('Bepado\SDK\ShippingCosts\RulesVisitor');
+        $visitor->visit(new Rules(array('rules' => array(
+            new Rule\CountryDecorator(array(
+                'delegatee' => new Rule\FreeCarriageLimit(array(
+                    'delegatee' => new Rule\WeightDecorator(array(
+                        'delegatee' => new Rule\FixedPrice()
+                    ))
+                ))
+            )),
+            new Rule\UnitPrice(),
+        ))));
+
+        $p = Phake::anyParameters();
+
+        Phake::inOrder(
+            Phake::verify($visitor)->startVisitRules($p),
+            Phake::verify($visitor, \Phake::times(2))->startVisitRule($p),
+            Phake::verify($visitor)->visitFreeCarriageLimit($p),
+            Phake::verify($visitor)->visitWeightDecorator($p),
+            Phake::verify($visitor)->visitFixedPrice($p),
+            Phake::verify($visitor, \Phake::times(2))->stopVisitRule($p),
+            Phake::verify($visitor, \Phake::times(2))->startVisitRule($p),
+            Phake::verify($visitor)->visitUnitPrice($p),
+            Phake::verify($visitor, \Phake::times(2))->stopVisitRule($p),
+            Phake::verify($visitor)->stopVisitRules($p)
+        );
+    }
+}
