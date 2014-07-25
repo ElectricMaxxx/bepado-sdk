f995366b615f984c58277c4177767a940e6b5d26
Kore Nordmann <mail@kore-nordmann.de>
2014-07-25 15:22:36 +0200

    Added legacy order converter to active converters

--- src/main/Bepado/SDK/DependencyResolver.php
+++ src/main/Bepado/SDK/DependencyResolver.php
@@ -409,7 +409,10 @@ class DependencyResolver
         if ($this->marshaller === null) {
             $this->marshaller = new Rpc\Marshaller\CallMarshaller\XmlCallMarshaller(
                 new \Bepado\SDK\XmlHelper(),
-                new Rpc\Marshaller\Converter\ExceptionToErrorConverter()
+                new Rpc\Marshaller\Converter\ChainingConverter(array(
+                    new Rpc\Marshaller\Converter\ExceptionToErrorConverter(),
+                    new Rpc\Marshaller\Converter\LegacyOrderConverter(),
+                ))
             );
         }
 
