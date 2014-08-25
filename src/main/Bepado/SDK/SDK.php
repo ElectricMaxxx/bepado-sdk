d818d94a406486a87d06d8babf6fbd46e822e0e9
Benjamin Eberlei <kontakt@beberlei.de>
2014-08-25 09:01:35 +0200

    Release 1.5.61

--- src/main/Bepado/SDK/SDK.php
+++ src/main/Bepado/SDK/SDK.php
@@ -51,7 +51,7 @@ final class SDK
     /**
      * Version constant
      */
-    const VERSION = '$Revision$';
+    const VERSION = '1.5.61';
 
     /**
      * @param string $apiKey API key assigned to you by Bepado
